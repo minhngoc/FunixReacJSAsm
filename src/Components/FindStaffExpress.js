e17f8b5eb644b866ad3ef36fab0a5c234130f901
Nguyen Minh Ngoc <ngocnm2701@gmail.com>
2022-02-19 19:16:20 +0700

    update salary format

--- src/Components/FindStaffExpress.js
+++ src/Components/FindStaffExpress.js
@@ -5,15 +5,20 @@ import dateFormat from 'dateformat';
 
 function FindStaffExpress() {
     const { userName }  = useParams();
-    const staff = STAFFS.find(item => item.name.toString() === userName.toString())
+    console.log(userName)
+    const staff = STAFFS.find(item => item.name.toString() === userName.replace('%20', ' '))
     return(
        <div>
-            <p>Họ và tên: {staff.name}</p>
-            <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
-            <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
-            <p>Phòng ban: {staff.department}</p>
-            <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
-            <p>Số ngày đã làm thêm: {staff.overTime}</p>
+            {staff && 
+                <div>
+                    <p>Họ và tên: {staff.name}</p>
+                    <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
+                    <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
+                    <p>Phòng ban: {staff.department}</p>
+                    <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+                    <p>Số ngày đã làm thêm: {staff.overTime}</p>
+                </div>
+            }
        </div>
     )
 }
